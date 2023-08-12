554bfde7636f7bb50db991ff4f8019ea9b8a5fb5
md shamim hossain <shamim.vc@gmail.com>
2023-08-12 14:16:17 +0600

    ga and gtm added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,18 +8,8 @@ export default function Document() {
 				<link rel="manifest" href="/manifest.json" />
 				<link rel="apple-touch-icon" href="/icon.png"></link>
 				<meta name="theme-color" content="#204971" />
-				<script async src="https://www.googletagmanager.com/gtag/js?id=G-PZKWXLXX9M"></script>
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
-							(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-							'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-							})(window,document,'script','dataLayer','GTM-P7GTVSGR');
-              			`,
-					}}
-				/>
+				<script async src="https://www.googletagmanager.com/gtag/js?id=G-M0RMS6EH9B"></script>
+
 
 				<script
 					dangerouslySetInnerHTML={{
@@ -27,8 +17,19 @@ export default function Document() {
 							window.dataLayer = window.dataLayer || [];
 							function gtag(){dataLayer.push(arguments);}
 							gtag('js', new Date());
-							gtag('config', 'G-PZKWXLXX9M');
-							gtag('event', 'gtagjsloaded', {'name':'hmshamim'});
+							gtag('config', 'G-M0RMS6EH9B');
+              			`,
+					}}
+				/>
+
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+							(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+							'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+							})(window,document,'script','dataLayer','GTM-P93H6N5Q');
               			`,
 					}}
 				/>
@@ -38,8 +39,9 @@ export default function Document() {
 				<Main />
 				<NextScript />
 
-				<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P7GTVSGR"
-					height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+				<noscript>
+					<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P93H6N5Q"
+						height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
 				</noscript>
 			</body>
 		</Html>
